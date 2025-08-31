08c56a145c00a6e4c13da727baade58b92a927aa
Oscar Darce <oscardarce@gmail.com>
2025-08-31 15:18:39 -0600

    Pushed deployment GiphyGifhub in Netlify

--- src/GifsApp.tsx
+++ src/GifsApp.tsx
@@ -1,34 +1,14 @@
-import { useState } from "react"
 
 import GifList from "./gifs/GifsList"
+import { useGifs } from "./gifs/hooks/useGifs"
 import PreviousSearches from "./gifs/PreviousSearches"
 import Header from "./shared/components/Header"
 import Searchbar from "./shared/components/Searchbar"
-import getGifsByQuery from "./gifs/actions/get-gifs-by-query.actions"
-import type { Gif } from "./gifs/interfaces/gif.interface"
+
 
 const GifsApp = () => {
 
-  const [gifs, setGifs] = useState<Gif[]>([])
-  const [previousSearches, setPreviousSearches] = useState<string[]>([])
-
-  const handleClickedTerms = (term: string) => {
-    console.log({ term })
-  }
-
-  const handleSearchBar = async (query: string = "") => {
-    query = query.trim().toLowerCase();
-
-    if (query.length === 0) return
-
-    if (previousSearches.includes(query)) return
-
-    setPreviousSearches([query, ...previousSearches].slice(0, 5))
-
-    const gifs = await getGifsByQuery(query)
-
-    setGifs(gifs)
-  }
+  const { gifs, previousSearches, handleClickedTerms, handleSearchBar } = useGifs()
 
   return (
     <>
